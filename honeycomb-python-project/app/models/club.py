751edd403012f35d62ce329c370432dd2f18142b
Jia Cui <cuijiajanae@gmail.com>
2021-07-17 11:18:15 -0700

    test

---
+++ honeycomb-python-project/app/models/club.py
@@ -0,0 +1,34 @@
+from .db import db
+from .user_club import user_club
+
+
+class Club(db.Model):
+    __tablename__ = "clubs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.Text, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        'categories.id'), nullable=False)
+    host_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    school_id = db.Column(db.Integer, db.ForeignKey(
+        'schools.id'), nullable=False)
+
+    categories = db.relationship('Category', back_populates="clubs")
+    host = db.relationship('User', back_populates="hosted_clubs")
+    schools = db.relationship('School', back_populates="clubs")
+    events = db.relationship('Event', back_populates="clubs")
+    users = db.relationship('User', secondary=user_club,
+                            back_populates="clubs")
+
+    def to_dict(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'img_url': self.img_url,
+            'category_id': self.category_id,
+            'host_id': self.host_id,
+            'school_id': self.school_id,
+        }
---
+++ honeycomb-python-project/app/models/event.py
@@ -0,0 +1,28 @@
+from .db import db
+
+
+class Event(db.Model):
+    __tablename__ = 'events'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date_and_time = db.Column(db.DateTime, nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    host_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    club_id = db.Column(db.Integer, db.ForeignKey('clubs.id'), nullable=False)
+
+    users = db.relationship('User', back_populates="events")
+    clubs = db.relationship('Club', back_populates='events')
+    rsvps = db.relationship('Rsvp', back_populates="rsvps")
+
+    def to_dict(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'date_and_time': self.date_and_time,
+            'location': self.location,
+            'host_id': self.host_id,
+            'club_id': self.club_id,
+        }
