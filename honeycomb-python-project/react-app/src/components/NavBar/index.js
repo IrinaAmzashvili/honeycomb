dab43be21ffe55be5327283668de2d82538c5384
Timothy Wieser <timothyswieser@gmail.com>
2021-07-18 17:27:21 -0400

    fixed cap issue on navbar

--- honeycomb-python-project/react-app/src/components/NavBar/index.js
+++ honeycomb-python-project/react-app/src/components/NavBar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
-// import styles from './NavBar.module.css'
+import styles from './Navbar.module.css'
 
 const NavBar = () => {
   return (
