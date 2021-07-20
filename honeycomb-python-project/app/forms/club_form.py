131ba1c652f6863d45c6dca850c7dfe0d80bbcea
Timothy Wieser <timothyswieser@gmail.com>
2021-07-19 20:00:42 -0400

    debugging club post

--- honeycomb-python-project/app/forms/club_form.py
+++ honeycomb-python-project/app/forms/club_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField
 from wtforms.validators import DataRequired
 from app.models import Club
 
