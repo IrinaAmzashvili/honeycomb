2bd164d83ebcc44dad4a40ee53d18c79c55ad4c8
Jia Cui <cuijiajanae@gmail.com>
2021-07-21 01:52:41 -0700

    finished read events and calendar

--- honeycomb-python-project/react-app/src/components/EventCards/index.js
+++ honeycomb-python-project/react-app/src/components/EventCards/index.js
@@ -1,20 +1,22 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
-
+import styles from '../EventCards/EventCards.module.css'
 
 
 const EventsCard = ({ event }) => {
 
     return (
-        <div>
-            <div>{event.date_and_time}</div>
-            <div>Location: {event.location}</div>
-            <div>{event.name}</div>
-            <div>{event.description}</div>
-            <div>
-                <button>Edit Event</button>
-                <button>Attend</button>
+        <div className={styles.outerContainer}>
+            <div className={styles.content}>
+                <div className={styles.eventDate}>{event.date_and_time}</div>
+                <div className={styles.eventLocation}>Location: {event.location}</div>
+                <div className={styles.eventName}>{event.name}</div>
+                <div className={styles.eventDescription}>{event.description}</div>
+            </div>
+            <div className={styles.buttonGroup}>
+                <button className={styles.editButton}>Edit Event</button>
+                <button className={styles.attendButton}>Attend</button>
             </div>
         </div>
     )
--- honeycomb-python-project/react-app/src/components/IndividualClub/IndividualClub.js
+++ honeycomb-python-project/react-app/src/components/IndividualClub/IndividualClub.js
@@ -1,11 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { getSingleClub } from '../../store/clubs';
 import styles from './IndividualClub.module.css'
 import { getEvents } from '../../store/events';
 import EventsCard from '../EventCards'
+import '@zach.codes/react-calendar/dist/calendar-tailwind-no-reset.css';
+import { format } from "date-fns";
 
+
+import {
+    MonthlyBody,
+    MonthlyCalendar,
+    MonthlyNav,
+    MonthlyDay,
+    DefaultMonthlyEventItem,
+} from '@zach.codes/react-calendar';
+
+const subHours = (date, hour) => {
+    return date.setHours(date.getHours() - hour);
+}
 
 const IndividualClub = () => {
     const sessionUser = useSelector(state => state.session.user)
@@ -13,6 +27,8 @@ const IndividualClub = () => {
     const club = useSelector(state => state.clubs.singleClub)
     // const club = useSelector(state => state.club)
     const dispatch = useDispatch()
+
+
 
 
     // ---------------------------------------Individual Club----------------------
@@ -27,7 +43,25 @@ const IndividualClub = () => {
 
     const events = useSelector(state => Object.values(state.events))
 
+    // ---------------------------------------calender----------------------
+    let [currentMonth, setCurrentMonth] = useState(
+        new Date()
+    );
 
+
+    const calendarEvents = () => {
+        let list = []
+        for (const event of events) {
+            let obj = {}
+            obj["title"] = event.name
+            obj["date"] = new Date(event.date_and_time)
+            list.push(obj)
+        }
+        return list
+
+    }
+
+    // const event = events.map((event) => event)
     return (
         <>
             <div className={styles.redCrossBar}>
@@ -52,12 +86,45 @@ const IndividualClub = () => {
                 </div>
 
             </div>
-            <div>
-                {events.map((event) => (
-                    <EventsCard event={event} />
-                ))}
+            <div className={styles.eventsAndCalender}>
+                <div className={styles.eventCardsContainer}>
+                    {events.map((event) => (
+                        <EventsCard event={event} />
+                    ))}
+                </div>
+                <div className={styles.calenderContainer}>
+                    <MonthlyCalendar
+                        className={styles.monthlyCalender}
+                        currentMonth={currentMonth}
+                        onCurrentMonthChange={date => setCurrentMonth(date)}
+                    >
+                        <MonthlyNav />
+                        <MonthlyBody
+                            // events={[
+                            //     { title: 'Call John', date: subHours(new Date(), 2) },
+                            //     { title: 'Call John', date: subHours(new Date(), 1) },
+                            //     { title: 'Meeting with Bob', date: new Date() },
+                            // ]}
+                            events={
+                                calendarEvents()
+                            }
+                        >
+                        <MonthlyDay className={styles.monthlyDay}
+                            renderDay={data =>
+                                data.map((item, index) => (
+                                <DefaultMonthlyEventItem
+                                    key={index}
+                                    title={item.title}
+                                    // Format the date here to be in the format you prefer
+                                    date={format(item.date, 'HH:mm')}
+                        />
+                        ))
+                    }
+                    />
+                        </MonthlyBody>
+                    </MonthlyCalendar>
+                </div>
             </div>
-
         </>
     )
 }
