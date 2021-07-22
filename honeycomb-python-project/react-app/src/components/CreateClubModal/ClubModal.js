9a9b5dfe6301aec12bdfb8ba4cc81ce71e59b12b
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-07-21 23:08:45 -0600

    Implement edit club modal styling

--- honeycomb-python-project/react-app/src/components/CreateClubModal/ClubModal.js
+++ honeycomb-python-project/react-app/src/components/CreateClubModal/ClubModal.js
@@ -1,89 +1,117 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { postClub } from "../../store/clubs";
-import styles from './ClubModal.module.css';
-
+import styles from "./ClubModal.module.css";
 
 function CreateClub({ setShowModal }) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [imgUrl, setImgUrl] = useState('');
-    const [category, setCategory] = useState("")
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  const [category, setCategory] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const newClub = {
+      name,
+      description,
+      img_url: imgUrl,
+      category_id: category,
+    };
+    dispatch(postClub(newClub));
+    setShowModal(false);
+  };
 
-        const newClub = {
-            name,
-            description,
-            img_url: imgUrl,
-            category_id: category,
-
-        }
-        dispatch(postClub(newClub))
-        setShowModal(false);
-    }
-
-    return (
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.club__heading_container}>
+          <h1 className={styles.club__form__heading}>Create A Club</h1>
+        </div>
+        <div className={styles.club__label__container}>
+          <label for="name" className={styles.club__form__label}>
+            Club Name
+          </label>
+        </div>
         <div>
-            <form onSubmit={handleSubmit}>
-                <div className={styles.club__heading_container}>
-                    <h1 className={styles.club__form__heading}>Create A Club</h1>
-                </div>
-                <div className={styles.club__label__container}>
-                    <label className={styles.club__form__label}>Club Name</label>
-                </div>
-                <div>
-                    <input className={styles.club__name} name="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                </div>
-
-
-                <div className={styles.club__label__container}>
-                    <label className={styles.club__form__label}>Club Image URL</label>
-                </div>
-                <div>
-                    <input className={styles.club__name} name="imgUrl" type="text" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)} />
-                </div>
-
-
-                <div className={styles.club__label__container}>
-                    <label className={styles.club__form__label}>Category</label>
-                </div>
-                <div>
-                    <select className={styles.club__name} value={category} onChange={(e) => setCategory(e.target.value)}>
-                        <option value="" disabled>Select a Category</option>
-                        <option value={1} >Social</option>
-                        <option value={2} >Academic</option>
-                        <option value={3} >Political</option>
-                        <option value={4} >Theater and Art</option>
-                        <option value={5} >Cultural</option>
-                        <option value={6} >Sports and Recreation</option>
-                        <option value={7} >Religious</option>
-                        <option value={8} >Community Service</option>
-                        <option value={9} >Media and Publication</option>
-                    </select>
-                </div>
-
-
-                <div className={styles.club__label__container}>
-                    <label className={styles.club__form__label}>Description</label>
-                </div>
-                <div>
-                    <textarea className={styles.club__description} name="description" value={description} onChange={(e) => setDescription(e.target.value)} />
-                </div>
-
-
-                <div>
-                    <button className={styles.submit_button} type="submit">Submit Club</button>
-                </div>
-            </form>
+          <input
+            id="name"
+            className={styles.club__name}
+            name="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-    )
+        <div className={styles.club__label__container}>
+          <label for="imgUrl" className={styles.club__form__label}>
+            Club Image URL
+          </label>
+        </div>
+        <div>
+          <input
+            id="imgUrl"
+            className={styles.club__name}
+            name="imgUrl"
+            type="text"
+            value={imgUrl}
+            onChange={(e) => setImgUrl(e.target.value)}
+          />
+        </div>
 
+        <div className={styles.club__label__container}>
+          <label for="category" className={styles.club__form__label}>
+            Category
+          </label>
+        </div>
+        <div>
+          <select
+            id="category"
+            className={styles.club__name}
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="" disabled>
+              Select a Category
+            </option>
+            <option value={1}>Social</option>
+            <option value={2}>Academic</option>
+            <option value={3}>Political</option>
+            <option value={4}>Theater and Art</option>
+            <option value={5}>Cultural</option>
+            <option value={6}>Sports and Recreation</option>
+            <option value={7}>Religious</option>
+            <option value={8}>Community Service</option>
+            <option value={9}>Media and Publication</option>
+          </select>
+        </div>
+
+        <div className={styles.club__label__container}>
+          <label for="description" className={styles.club__form__label}>
+            Description
+          </label>
+        </div>
+        <div>
+          <textarea
+            id="description"
+            className={styles.club__description}
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button className={styles.submit_button} type="submit">
+            Submit Club
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default CreateClub
+export default CreateClub;
--- honeycomb-python-project/react-app/src/components/EditClubForm/EditClubForm.js
+++ honeycomb-python-project/react-app/src/components/EditClubForm/EditClubForm.js
@@ -1,57 +1,108 @@
-import React, { useState } from 'react';
-import styles from './EditClub.module.css';
-import { editClub } from '../../store/clubs';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import styles from '../CreateClubModal/ClubModal.module.css'
+import { editClub } from "../../store/clubs";
+import { useDispatch } from "react-redux";
 
 const EditClubForm = ({ club, setShowModal }) => {
-    const dispatch = useDispatch();
-    const [name, setName] = useState(club.name);
-    const [description, setDescription] = useState(club.description);
-    const [imgUrl, setImgUrl] = useState(club.img_url);
-    const [category, setCategory] = useState(club.category_id);
+  const dispatch = useDispatch();
+  const [name, setName] = useState(club.name);
+  const [description, setDescription] = useState(club.description);
+  const [imgUrl, setImgUrl] = useState(club.img_url);
+  const [category, setCategory] = useState(club.category_id);
 
+  const editForm = (event) => {
+    event.preventDefault();
 
-    const editForm = (event) => {
-        event.preventDefault();
+    const editedFormInfo = {
+      name,
+      description,
+      img_url: imgUrl,
+      category_id: category,
+    };
+    dispatch(editClub(club.id, editedFormInfo));
+    setShowModal(false);
+  };
 
-        const editedFormInfo = {
-            name,
-            description,
-            img_url: imgUrl,
-            category_id: category
-        }
-        dispatch(editClub(club.id, editedFormInfo));
-        setShowModal(false)
-    }
-
-    return (
-        <>
-            <div className={styles.editFormContainer}>
-                <form onSubmit={editForm} className={styles.editForm}>
-                    <div className={styles.editHeadingContainer}>
-                        <h1 className={styles.editHeading}>Edit Form</h1>
-                    </div>
-                    <input name="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} type='text' className={styles}/>
-                    <input name="img_url" placeholder="Image Url" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)} className={styles}/>
-                    <textarea name="description" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} type='text' className={styles}></textarea>
-                    <select name="category" value={category} onChange={(e) => setCategory(e.target.value)} className={styles}>
-                        <option value="" disabled>Select a Category</option>
-                        <option value={1} >Social</option>
-                        <option value={2} >Academic</option>
-                        <option value={3} >Political</option>
-                        <option value={4} >Theater and Art</option>
-                        <option value={5} >Cultural</option>
-                        <option value={6} >Sports and Recreation</option>
-                        <option value={7} >Religious</option>
-                        <option value={8} >Community Service</option>
-                        <option value={9} >Media and Publication</option>
-                    </select>
-                <button className={styles.submitEditButton}>Edit Club</button>
-                </form>
-            </div>
-        </>
-    )
-}
-
+  return (
+      <div>
+        <form onSubmit={editForm}>
+          <div className={styles.club__heading_container}>
+            <h1 className={styles.club__form__heading}>Edit Form</h1>
+          </div>
+          <div className={styles.club__label__container}>
+            <label for="name" className={styles.club__form__label}>
+              Club Name
+            </label>
+          </div>
+          <div>
+            <input
+              name="name"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              className={styles.club__name}
+            />
+          </div>
+          <div className={styles.club__label__container}>
+            <label for="imgUrl" className={styles.club__form__label}>
+              Club Image URL
+            </label>
+          </div>
+          <div>
+            <input
+              name="img_url"
+              placeholder="Image Url"
+              value={imgUrl}
+              onChange={(e) => setImgUrl(e.target.value)}
+              className={styles.club__name}
+            />
+          </div>
+          <div className={styles.club__label__container}>
+            <label for="category" className={styles.club__form__label}>
+              Category
+            </label>
+          </div>
+          <div>
+            <select
+              name="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className={styles.club__name}
+            >
+              <option value="" disabled>
+                Select a Category
+              </option>
+              <option value={1}>Social</option>
+              <option value={2}>Academic</option>
+              <option value={3}>Political</option>
+              <option value={4}>Theater and Art</option>
+              <option value={5}>Cultural</option>
+              <option value={6}>Sports and Recreation</option>
+              <option value={7}>Religious</option>
+              <option value={8}>Community Service</option>
+              <option value={9}>Media and Publication</option>
+            </select>
+          </div>
+          <div className={styles.club__label__container}>
+            <label for="description" className={styles.club__form__label}>
+              Description
+            </label>
+          </div>
+          <div>
+            <textarea
+              name="description"
+              placeholder="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              type="text"
+              className={styles.club__description}
+            ></textarea>
+          </div>
+          <button className={styles.submit_button}>Edit Club</button>
+        </form>
+      </div>
+  );
+};
 
 export default EditClubForm;
