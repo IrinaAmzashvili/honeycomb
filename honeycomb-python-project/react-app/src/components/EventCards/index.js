c346b4493850e6709d0161939f939a2ff199b03e
Machaelmus <michaeljtufo@gmail.com>
2021-07-21 20:10:07 -0400

    Added totality of members attending one event

--- honeycomb-python-project/react-app/src/components/EventCards/index.js
+++ honeycomb-python-project/react-app/src/components/EventCards/index.js
@@ -6,6 +6,10 @@ import styles from '../EventCards/EventCards.module.css'
 
 const EventsCard = ({ event }) => {
 
+    // console.log('Find the event members through this console log',event)
+    // const getRsvps = () => {
+    //     return event.rsvps.length;
+    // }
     const newTime = new Date(event.date_and_time)
 
     return (
@@ -13,6 +17,7 @@ const EventsCard = ({ event }) => {
             <div className={styles.content}>
                 <div className={styles.eventDate}>{newTime.toLocaleDateString()}{newTime.toLocaleTimeString()}</div>
                 {/* <div className={styles.eventDate}>{event.date_and_time}</div> */}
+                <div className={styles.eventRsvps}>There are {event.rsvps.length !== 0 ? ` ${event.rsvps.length} members attending` : ' no members attending'}</div>
                 <div className={styles.eventLocation}>Location: {event.location}</div>
                 <div className={styles.eventName}>{event.name}</div>
                 <div className={styles.eventDescription}>{event.description}</div>
