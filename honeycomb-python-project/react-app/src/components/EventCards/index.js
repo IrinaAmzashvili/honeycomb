ed55ab2ecef14f9bb750a75d36db5c6352d2c655
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-07-22 09:25:31 -0600

    Cleanup some react warnings

--- honeycomb-python-project/react-app/src/components/EventCards/index.js
+++ honeycomb-python-project/react-app/src/components/EventCards/index.js
@@ -1,6 +1,3 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux';
 import styles from '../EventCards/EventCards.module.css'
 
 
