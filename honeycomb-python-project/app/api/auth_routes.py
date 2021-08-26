028f6342640bbabd573d02b5030cf62d6cec37a9
Timothy Wieser <timothyswieser@gmail.com>
2021-08-26 14:57:27 -0400

    final touches on counter

--- honeycomb-python-project/app/api/auth_routes.py
+++ honeycomb-python-project/app/api/auth_routes.py
@@ -77,8 +77,7 @@ def sign_up():
         db.session.commit()
         login_user(user)
         return user.to_dict()
-    print('===================>', {
-          'errors': validation_errors_to_error_messages(form.errors)})
+
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
